caecbfd238bca389a46c4db0bddd48064cef66bc
Pete Ware <ware@peteware.com>
2019-11-21 20:06:31 -0500

    Adding pmr

--- pw_vector.h
+++ pw_vector.h
@@ -2,6 +2,7 @@
 #define PW_VECTOR_H
 
 #include <pw_allocator.h>
+#include <pw_pmr_allocator.h>
 
 namespace pw {
 
@@ -10,6 +11,12 @@ class vector
 {
 public:
 };
+
+namespace pmr {
+template <class Type>
+using vector = pw::vector<Type, pw::pmr::polymorphic_allocator<Type> >;
+}
+
 }
 
 #endif /*  PW_VECTOR_H */
