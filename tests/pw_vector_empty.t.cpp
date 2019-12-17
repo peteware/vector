2162d90dc950e9d4bbec7035ef2bad88c563e3a6
Pete Ware <ware@peteware.com>
2019-12-16 23:43:56 -0500

    Adding allocator to vector

--- tests/pw_vector_empty.t.cpp
+++ tests/pw_vector_empty.t.cpp
@@ -24,11 +24,31 @@
 
 TEMPLATE_TEST_CASE("empty vectors work", "[vector][template]", int, std::string)
 {
-    pw::vector<TestType> v;
+    GIVEN("An empty vector of TestType")
+    {
+        pw::vector<TestType> v;
+        WHEN("nothing changes")
+        {
+            THEN("Check the types are expected")
+            {
+                REQUIRE(pw::is_same<TestType*, typename pw::vector<TestType>::pointer>::value);
+                REQUIRE(pw::is_same<TestType, typename pw::vector<TestType>::value_type>::value);
+                REQUIRE(v.empty());
+                REQUIRE(v.size() == 0);
+                REQUIRE(v.capacity() == 0);
+            }
+        }
 
-    REQUIRE(pw::is_same<TestType*, typename pw::vector<TestType>::pointer>::value);
-    REQUIRE(pw::is_same<TestType, typename pw::vector<TestType>::value_type>::value);
-    REQUIRE(v.empty());
-    REQUIRE(v.size() == 0);
-    REQUIRE(v.capacity() == 0);
+        WHEN("One item is push_back()'d")
+        {
+            v.push_back(TestType());
+
+            THEN("Size has changed")
+            {
+                REQUIRE(!v.empty());
+                REQUIRE(v.size() == static_cast<typename pw::vector<TestType>::size_type>(1));
+                REQUIRE(v.capacity() >= v.size());
+            }
+        }
+    }
 }
