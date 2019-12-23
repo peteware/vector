a0ee9271ed5d010c00628029d7f3e9a99a0dc480
Pete Ware <pware@bloomberg.net>
2019-12-23 17:28:21 -0500

    Adds fill()

---
+++ pw/internal/fill.h
@@ -0,0 +1,15 @@
+#ifndef _INCLUDED_PW_INTERNAL_FILL_H_
+#define _INCLUDED_PW_INTERNAL_FILL_H_
+namespace pw { namespace internal {
+template<class ForwardIterator, class Type>
+void
+fill(ForwardIterator begin, ForwardIterator end, Type const& value)
+{
+    while (begin != end)
+    {
+        *begin++ = value;
+    }
+}
+
+}} // namespace pw::internal
+#endif /*  _INCLUDED_PW_INTERNAL_FILL_H_ */
