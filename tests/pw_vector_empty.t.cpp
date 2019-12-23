b5f2847f849a36c0f0041d17f850b039ae9dee25
Pete Ware <pware@bloomberg.net>
2019-12-23 14:19:17 -0500

    Adding at() and unit tests

--- tests/pw_vector_empty.t.cpp
+++ tests/pw_vector_empty.t.cpp
@@ -1,7 +1,8 @@
-#include <pw/vector>
-#include <pw/type_traits>
 #include "catch2/catch.hpp"
+#include <pw/type_traits>
+#include <pw/vector>
 
+#include <stdexcept>
 #include <string>
 
 // SCENARIO( "vectors can be sized and resized", "[vector]" ) {
@@ -33,22 +34,50 @@ TEMPLATE_TEST_CASE("empty vectors work", "[vector][template]", int, std::string)
             {
                 REQUIRE(pw::is_same<TestType*, typename pw::vector<TestType>::pointer>::value);
                 REQUIRE(pw::is_same<TestType, typename pw::vector<TestType>::value_type>::value);
+            }
+            THEN("It is reported as empty")
+            {
                 REQUIRE(v.empty());
                 REQUIRE(v.size() == 0);
                 REQUIRE(v.capacity() == 0);
             }
-        }
-
-        WHEN("One item is push_back()'d")
-        {
-            v.push_back(TestType());
-
-            THEN("Size has changed")
+            THEN("at() fails")
             {
-                REQUIRE(!v.empty());
-                REQUIRE(v.size() == static_cast<typename pw::vector<TestType>::size_type>(1));
-                REQUIRE(v.capacity() >= v.size());
+                THEN("at(0) fails") { CHECK_THROWS_AS(v.at(0), std::out_of_range); }
+                THEN("at(10) fails") { CHECK_THROWS_AS(v.at(10), std::out_of_range); }
             }
+        }
+    }
+    GIVEN("A vector of TestType with 1 item")
+    {
+        pw::vector<TestType> v;
+        v.push_back(TestType());
+        WHEN("size is checked")
+        {
+            size_t s = v.size();
+            THEN("size is 1") { REQUIRE(s == 1); }
+        }
+        WHEN("empty is checked")
+        {
+            bool e = v.empty();
+            THEN("it is not empty") { REQUIRE(!e); }
+        }
+        WHEN("capacity is checked")
+        {
+            size_t c = v.capacity();
+            THEN("it is at least 1)") { REQUIRE(c >= 1); }
+        }
+        WHEN("at(0) is called")
+        {
+            THEN("it works")
+            {
+                TestType& r = v.at(0);
+                REQUIRE(r == TestType());
+            }
+        }
+        WHEN("at(1) is called")
+        {
+            THEN("it raises exception") { CHECK_THROWS_AS(v.at(1), std::out_of_range); }
         }
     }
 }
