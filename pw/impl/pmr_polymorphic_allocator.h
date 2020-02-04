59ad0ab4aa9e923fec4f9d5c0798f33c097f4b33
Pete Ware <ware@peteware.com>
2020-02-03 22:16:21 -0500

    More re-arranging

---
+++ pw/impl/pmr_polymorphic_allocator.h
@@ -0,0 +1,13 @@
+#ifndef _INCLUDED_PW_PMR_POLYMORPHIC_ALLOCATOR_H
+#define _INCLUDED_PW_PMR_POLYMORPHIC_ALLOCATOR_H
+
+namespace pw { namespace pmr {
+template<class Type>
+class polymorphic_allocator
+{
+public:
+    using value_type = Type;
+};
+}} // namespace pw::pmr
+
+#endif /*  _INCLUDED_PW_PMR_POLYMORPHIC_ALLOCATOR_H */
