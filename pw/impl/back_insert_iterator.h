73e2da4ed5492fcb3e4fc4c9918868b2052c23fc
Pete Ware <ware@peteware.com>
2020-03-04 22:02:41 -0500

    Adding back_insert_iterator

--- pw/impl/back_insert_iterator.h
+++ pw/impl/back_insert_iterator.h
@@ -2,9 +2,11 @@
 #define INCLUDED_IMPL_BACK_INSERT_ITERATOR_H
 
 #include <pw/impl/iterator_tag.h>
+#include <pw/impl/move.h>
 
 namespace pw {
 
+template<class Container>
 class back_insert_iterator
 {
 public:
@@ -15,9 +17,58 @@ public:
     using reference         = void;
     using container_type    = Container;
 
+    explicit back_insert_iterator(container_type& container);
+
+    back_insert_iterator<Container>& operator=(const typename Container::value_type& value);
+    back_insert_iterator<Container>& operator=(typename Container::value_type&& value);
+    back_insert_iterator<Container>& operator*();
+    back_insert_iterator<Container>& operator++();
+    back_insert_iterator<Container>& operator++(int);
+
 protected:
     container_type* m_container;
 };
+
+template<class Container>
+back_insert_iterator<Container>::back_insert_iterator(container_type& container)
+{
+}
+
+template<class Container>
+back_insert_iterator<Container>&
+back_insert_iterator<Container>::operator=(const typename Container::value_type& value)
+{
+    m_container->push_back(value);
+    return *this;
+}
+
+template<class Container>
+back_insert_iterator<Container>&
+back_insert_iterator<Container>::operator=(typename Container::value_type&& value)
+{
+    m_container->push_back(pw::move(value));
+    return *this;
+}
+
+template<class Container>
+back_insert_iterator<Container>& back_insert_iterator<Container>::operator*()
+{
+    return *this;
+}
+
+template<class Container>
+back_insert_iterator<Container>&
+back_insert_iterator<Container>::operator++()
+{
+    return *this;
+}
+
+template<class Container>
+back_insert_iterator<Container>&
+back_insert_iterator<Container>::operator++(int)
+{
+    return *this;
+}
 
 } // namespace pw
 
