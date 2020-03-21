bc96935397ab732a688d918f6d65102b6671555c
Pete Ware <ware@peteware.com>
2020-03-21 16:27:55 -0400

    Adding unit tests for reverse iterator

---
+++ tests/reverse_iterator.t.cpp
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_TESTS_REVERSE_ITERATOR_T_H
+#define INCLUDED_TESTS_REVERSE_ITERATOR_T_H
+
+#include "catch2/catch.hpp"
+
+#include <pw/impl/reverse_iterator.h>
+
+namespace tests {
+
+}
+
+#endif /* INCLUDED_TESTS_REVERSE_ITERATOR_T_H */
