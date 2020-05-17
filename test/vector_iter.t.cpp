f62b6af004ad109b13d988344463da3e4b6adffe
Pete Ware <ware@peteware.com>
2020-05-17 10:18:11 -0400

    Moving begin()/end() etc into own test file

---
+++ test/vector_iter.t.cpp
@@ -0,0 +1,73 @@
+#include <catch2/catch.hpp>
+#include <pw/algorithm>
+#include <test_permute.h>
+//#include <test_same.h>
+#include <test_testtype.h>
+//#include <pw/allocator>
+#include <pw/type_traits>
+#include <pw/vector>
+
+#include <stdexcept>
+
+TEMPLATE_LIST_TEST_CASE("iterator methods on empty vector", "[vector][iter][empty]", pw::test::TestTypeList)
+{
+    using Vector     = TestType;
+    using value_type = typename Vector::value_type;
+
+    GIVEN("An empty vector of TestType")
+    {
+        Vector v;
+        REQUIRE(pw::is_same<value_type*, typename Vector::pointer>::value);
+        REQUIRE(pw::is_same<value_type, typename Vector::value_type>::value);
+        WHEN("begin() is called")
+        {
+            typename Vector::iterator iter;
+            iter = v.begin();
+            THEN("cbegin() is same")
+            {
+                REQUIRE(iter == v.cbegin());
+            }
+            THEN("cend() is same")
+            {
+                REQUIRE(iter == v.begin());
+            }
+            THEN("end() is same")
+            {
+                REQUIRE(iter == v.end());
+            }
+            THEN("cend() is same as end()")
+            {
+                REQUIRE(iter == v.cend());
+            }
+        }
+        GIVEN("and it is const")
+        {
+            Vector const& c = v;
+            REQUIRE(c.empty());
+            REQUIRE(c.size() == 0);
+            REQUIRE(c.capacity() == 0);
+
+            WHEN("begin() is called")
+            {
+                typename Vector::const_iterator iter;
+                iter = v.begin();
+                THEN("cend() is same")
+                {
+                    REQUIRE(iter == v.cend());
+                }
+                THEN("cbegin() is same")
+                {
+                    REQUIRE(iter == v.cbegin());
+                }
+                THEN("end() is same")
+                {
+                    REQUIRE(iter == v.end());
+                }
+                THEN("cend() is same as end()")
+                {
+                    REQUIRE(iter == v.cend());
+                }
+            }
+        }
+    }
+}
