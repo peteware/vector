22e4049f1f07977cf4c82568bb7207a7b62c4d1e
Pete Ware <pware@bloomberg.net>
2021-02-15 14:10:37 -0500

    Changes to use pw::next() and pw::prev()
    This avoids raw pointer operations

---
+++ pw/impl/prev.h
@@ -0,0 +1,18 @@
+#ifndef INCLUDED_PW_IMPL_PREV_H
+#define INCLUDED_PW_IMPL_PREV_H
+
+#include <pw/impl/advance.h>
+#include <pw/impl/iterator_traits.h>
+
+namespace pw {
+
+template<class Iterator>
+constexpr Iterator
+prev(Iterator iterator, typename pw::iterator_traits<Iterator>::difference_type n = 1)
+{
+    pw::advance(iterator, -n);
+    return iterator;
+}
+
+} // namespace pw
+#endif /* INCLUDED_PW_IMPL_PREV_H */
