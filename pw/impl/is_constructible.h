8c2d0502fe34f439a4ba8b15be79c848705d97c4
Pete Ware <pware@bloomberg.net>
2021-02-23 08:49:08 -0500

    WIP: still working on is_constructible; busted right now

--- pw/impl/is_constructible.h
+++ pw/impl/is_constructible.h
@@ -4,10 +4,22 @@
 #include <pw/impl/bool_type.h>
 
 namespace pw {
-template<class Type, class... Args>
-struct is_constructible : public false_type
+
+template<class T>
+typename add_rvalue_reference<T>::type declval() noexcept;
+
+template<class, class Type, class... Args>
+struct is_constructible_ : pw::false_type
 {
 };
+
+template<class Type, class... Args>
+struct is_constructible_<void_t<decltype(Type(pw::declval<Args>()...))>, Type, Args...> : pw::true_type
+{
+};
+
+template<class Type, class... Args>
+using is_constructible = is_constructible_<void_t<>, Type, Args...>;
 
 template<class Type, class... Args>
 inline constexpr bool is_constructible_v = is_constructible<Type, Args...>::value;
