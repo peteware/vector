4fe2b36e9b8eb988acd95a60fdb9401858bb7baf
Pete Ware <pware@bloomberg.net>
2021-02-25 22:48:41 -0500

    Adds exchange()

---
+++ pw/impl/exchange.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDED_PW_IMPL_EXCHANGE_H
+#define INCLUDED_PW_IMPL_EXCHANGE_H
+
+#include <pw/impl/forward.h>
+#include <pw/impl/move.h>
+
+namespace pw {
+
+template<class Type, class OtherType = Type>
+constexpr Type
+exchange(Type& obj, OtherType&& new_value)
+{
+    Type old = pw::move(obj);
+    obj      = pw::forward<OtherType>(new_value);
+    return old;
+}
+
+} // namespace pw
+
+#endif /* INCLUDED_PW_IMPL_EXCHANGE_H */
---
+++ tests/unit/exchange.t.cpp
@@ -0,0 +1,35 @@
+#include <pw/impl/exchange.h>
+
+#include <string>
+
+#include <catch2/catch.hpp>
+
+SCENARIO("exchange", "[utility]")
+{
+    GIVEN("An int")
+    {
+        int val = 2;
+        WHEN("exchange(val, 3)")
+        {
+            int n = pw::exchange(val, 4);
+            THEN("they exchanged values")
+            {
+                REQUIRE(n == 2);
+                REQUIRE(val == 4);
+            }
+        }
+    }
+    GIVEN("A std::string")
+    {
+        std::string val("abc");
+        WHEN("exchange(val, 'def'")
+        {
+            std::string n = pw::exchange(val, "def");
+            THEN("they exchanged values")
+            {
+                REQUIRE(n == "abc");
+                REQUIRE(val == "def");
+            }
+        }
+    }
+}
