0487424e1c108d71be4bba33c0788c2b1e4d9cae
Pete Ware <ware@peteware.com>
2021-03-09 08:44:44 -0500

    WIP: does not compile

---
+++ pw/impl/fill_n.h
@@ -0,0 +1,17 @@
+#ifndef INCLUDED_PW_IMPL_FILL_N_H
+#define INCLUDED_PW_IMPL_FILL_N_H
+
+namespace pw {
+template<class Iterator, class Size, class Type>
+constexpr Iterator
+fill_n(Iterator first, Size count, const Type& value)
+{
+    for (Size i = 0; i < count; ++i)
+    {
+        *first++ = value;
+    }
+    return first;
+}
+
+} // namespace pw
+#endif /* INCLUDED_PW_IMPL_FILL_N_H */
