ee93df946f4263f86df57988a6c1b8bfae271986
Pete Ware <ware@peteware.com>
2021-09-26 22:15:13 -0400

    Stripped all code from vector to define minimal class
    This does not pass unit tests since there's no code

--- tests/unit/vector_assignment.t.cpp
+++ tests/unit/vector_assignment.t.cpp
@@ -1,5 +1,7 @@
 #include <test_testtype.h>
 
+#include <pw/impl/move.h>
+
 #include <catch2/catch.hpp>
 
 /*
@@ -18,75 +20,51 @@ TEMPLATE_LIST_TEST_CASE("Assignment operator", "[vector][operator=]", pw::test::
         {
             Vector op2;
             op2 = v;
-            THEN("equal")
-            {
-                REQUIRE(pw::equal(v.begin(), v.end(), op2.begin(), op2.end()));
-            }
+            THEN("equal") { REQUIRE(pw::equal(v.begin(), v.end(), op2.begin(), op2.end())); }
         }
         WHEN("operator=(const_ref) lhs has elements")
         {
             Vector op2(5);
             op2 = v;
-            THEN("equal")
-            {
-                REQUIRE(pw::equal(v.begin(), v.end(), op2.begin(), op2.end()));
-            }
+            THEN("equal") { REQUIRE(pw::equal(v.begin(), v.end(), op2.begin(), op2.end())); }
         }
         WHEN("operator=(const_ref) rhs has elements")
         {
             Vector op2(5);
             v = op2;
-            THEN("equal")
-            {
-                REQUIRE(pw::equal(v.begin(), v.end(), op2.begin(), op2.end()));
-            }
+            THEN("equal") { REQUIRE(pw::equal(v.begin(), v.end(), op2.begin(), op2.end())); }
         }
         WHEN("operator=(move) both empty")
         {
             Vector op2;
             op2 = pw::move(v);
-            THEN("size() is same")
-            {
-                REQUIRE(v.size() == op2.size());
-            }
+            THEN("size() is same") { REQUIRE(v.size() == op2.size()); }
         }
         WHEN("operator=(move) lhs has elements")
         {
             Vector op2(5);
             REQUIRE(op2.size() == 5);
             op2 = pw::move(v);
-            THEN("size() is same")
-            {
-                REQUIRE(op2.size() == 0);
-            }
+            THEN("size() is same") { REQUIRE(op2.size() == 0); }
         }
         WHEN("operator=(move) rhs has elements")
         {
             Vector op2(5);
             v = pw::move(op2);
-            THEN("size() is same")
-            {
-                REQUIRE(v.size() == 5);
-            }
+            THEN("size() is same") { REQUIRE(v.size() == 5); }
         }
         WHEN("operator=(initializer_list) lhs is empty")
         {
             v = { value_type(), value_type(), value_type() };
 
-            THEN("size() is 3")
-            {
-                REQUIRE(v.size() == 3);
-            }
+            THEN("size() is 3") { REQUIRE(v.size() == 3); }
         }
         WHEN("operator=(initializer_list) lhs has elements")
         {
             v.resize(10);
             v = { value_type(), value_type(), value_type() };
 
-            THEN("size() is 3")
-            {
-                REQUIRE(v.size() == 3);
-            }
+            THEN("size() is 3") { REQUIRE(v.size() == 3); }
         }
     }
 }
