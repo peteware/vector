1a58459b96d6f94bdebbf820a82f4f9cc70e919b
Pete Ware <ware@peteware.com>
2021-10-01 11:25:47 -0400

    Adds a test

--- tests/basicunit/vector_int.t.cpp
+++ tests/basicunit/vector_int.t.cpp
@@ -17,8 +17,19 @@ TEST_CASE("Constructors", "[vector][constructor]")
         WHEN("Check if empty")
         {
             bool empty = v.empty();
-            ;
             THEN("it is empty") { REQUIRE(empty); }
+        }
+    }
+    GIVEN("A vector of count values")
+    {
+        pw::size_t const total = 10;
+        value_type       value = 3;
+        Vector           v(total, value);
+
+        WHEN("Check not empty")
+        {
+            bool empty = v.empty();
+            THEN("not empty") { REQUIRE(!empty); }
         }
     }
 }
