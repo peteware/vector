b1aa1e9c1a51eaee0a6e7606050701e09c6c1979
Pete Ware <pware@bloomberg.net>
2025-08-03 10:45:14 -0400

    Add documentation for move_backward() function

--- pw/impl/move_backward.h
+++ pw/impl/move_backward.h
@@ -5,6 +5,21 @@
 
 namespace pw {
 
+/**
+ * Moves the elements from the range [begin, end) to another range ending at dest.
+ * The elements are moved in reverse order (the last element is moved first), 
+ * but their relative order is preserved.
+ *
+ * @param begin Input iterator to the first element of the range to move from
+ * @param end   Input iterator to one past the last element of the range to move from  
+ * @param dest  Output iterator to one past the last element of the destination range
+ *
+ * @return Iterator to the element that would precede the first element of the 
+ *         destination range (i.e., dest - (end - begin))
+ *
+ * @note The ranges [begin, end) and [dest - (end - begin), dest) must not overlap,
+ *       or if they overlap, dest must not be in the range [begin, end).
+ */
 template<class Iterator1, class Iterator2>
 Iterator2
 move_backward(Iterator1 begin, Iterator1 end, Iterator2 dest)
