99c110d50270dfc2d766949d673cf55016f5b11c
Pete Ware <pware@bloomberg.net>
2025-08-03 19:22:52 -0400

    Add documentation for move() function to clarify usage and parameters

--- pw/impl/move_alg.h
+++ pw/impl/move_alg.h
@@ -6,6 +6,20 @@
 
 namespace pw {
 
+/**
+ * Moves the elements in the range [begin, end) to another range beginning at dest.
+ * The elements are moved in order (the first element is moved first).
+ *
+ * @param begin Input iterator to the first element of the range to move from
+ * @param end   Input iterator to one past the last element of the range to move from
+ * @param dest  Output iterator to the beginning of the destination range
+ *
+ * @return Output iterator to the element in the destination range, one past
+ *         the last element moved (i.e., dest + (end - begin))
+ *
+ * @note The ranges [begin, end) and [dest, dest + (end - begin)) must not overlap,
+ *       or if they overlap, dest must not be in the range (begin, end].
+ */
 template<class InputIterator, class OutputIterator>
 OutputIterator
 move(InputIterator begin, InputIterator end, OutputIterator dest)
