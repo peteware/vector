d722e064ecfd3048b79d523516ca7ff392c57784
Pete Ware <pware@bloomberg.net>
2025-08-07 20:04:53 -0400

    Refactor vector constructor tests to use initializer lists and improve clarity

--- tests/unit/vector_constructor.t.cpp
+++ tests/unit/vector_constructor.t.cpp
@@ -1,199 +1,354 @@
-#include <pw/vector>
-#include <test_defaultcopyconstructible.h>
-#include <test_moveconstructible.h>
-#include <test_testtype.h>
-#include <test_values.h>
-
 #include <pw/algorithm>
+#include <pw/vector>
 
+#include <initializer_list>
 #include <tuple>
 #include <vector>
 
 #include <catch2/catch.hpp>
 
-using TestTypeList = std::tuple<pw::vector<pw::test::DefaultCopyConstructible>,
-                                std::vector<pw::test::DefaultCopyConstructible>>;
-TEMPLATE_LIST_TEST_CASE("vector() constructor", "[vector][constructor]", TestTypeList)
+// Phase 1 test type list - using int with pw::vector and std::vector for comparison
+using Phase1TestTypeList = std::tuple<pw::vector<int>, std::vector<int>>;
+
+TEMPLATE_LIST_TEST_CASE("Initializer list constructor",
+                        "[phase1][vector][constructor][init-list]",
+                        Phase1TestTypeList)
 {
     using Vector = TestType;
 
-    GIVEN("An empty vector")
+    GIVEN("An initializer list")
     {
-        pw::test::OpCounter       counter;
-        pw::test::OpCounter const init(pw::test::DefaultCopyConstructible::getCounter());
-        Vector                    v;
-
-        WHEN("nothing is done")
+        WHEN("constructed with empty initializer list")
         {
-            counter = pw::test::DefaultCopyConstructible::getCounter() - init;
-            THEN("Nothing was constructed")
+            Vector v{};
+
+            THEN("the vector is empty")
             {
-                REQUIRE(counter.zero());
+                REQUIRE(v.empty() == true);
+                REQUIRE(v.begin() == v.end());
+            }
+        }
+
+        WHEN("constructed with {1, 2, 3}")
+        {
+            Vector v{1, 2, 3};
+
+            THEN("the vector has the expected elements")
+            {
+                REQUIRE(v.size() == 3);
+                REQUIRE(v.empty() == false);
+                REQUIRE(v.capacity() >= 3);
+                REQUIRE(v[0] == 1);
+                REQUIRE(v[1] == 2);
+                REQUIRE(v[2] == 3);
+            }
+        }
+
+        WHEN("constructed with {42}")
+        {
+            Vector v{42};
+
+            THEN("the vector has one element")
+            {
+                REQUIRE(v.size() == 1);
+                REQUIRE(v.empty() == false);
+                REQUIRE(v.capacity() >= 1);
+                REQUIRE(v[0] == 42);
+            }
+        }
+
+        WHEN("constructed with {-1, 0, 1, -5, 100}")
+        {
+            Vector v{-1, 0, 1, -5, 100};
+
+            THEN("the vector has all the elements in order")
+            {
+                REQUIRE(v.size() == 5);
+                REQUIRE(v[0] == -1);
+                REQUIRE(v[1] == 0);
+                REQUIRE(v[2] == 1);
+                REQUIRE(v[3] == -5);
+                REQUIRE(v[4] == 100);
             }
         }
     }
 }
 
-/*
- * Type requirements:
- * - No extra
- */
-TEMPLATE_LIST_TEST_CASE("count constructors in vector", "[vector][constructor]", TestTypeList)
+TEMPLATE_LIST_TEST_CASE("Default constructor", "[phase1][vector][constructor][default]", Phase1TestTypeList)
 {
     using Vector = TestType;
 
-    pw::test::OpCounter       counter;
-    pw::test::OpCounter const init(pw::test::DefaultCopyConstructible::getCounter());
-
-    GIVEN("An empty vector")
+    GIVEN("A default constructed vector")
     {
-        Vector v;
+        WHEN("default constructed")
+        {
+            Vector v;
 
-        WHEN("Nothing was called")
-        {
-            counter = pw::test::DefaultCopyConstructible::getCounter() - init;
-            THEN("Nothing was constructed")
+            THEN("the vector is empty")
             {
-                REQUIRE(counter.zero());
-            }
-        }
-        WHEN("reserve() is increased")
-        {
-            counter = pw::test::DefaultCopyConstructible::getCounter();
-            v.reserve(5);
-            counter = pw::test::DefaultCopyConstructible::getCounter() - counter;
-            THEN("copy constructor not called")
-            {
-                REQUIRE(counter.zero());
+                Vector expected{};
+                REQUIRE(v.empty() == true);
+                REQUIRE(v.begin() == v.end());
+                REQUIRE(v == expected);
             }
         }
     }
-    GIVEN("A vector with 5 elements")
-    {
-        pw::test::Values<Vector> generate(5);
-        Vector                   v(generate.values);
-        REQUIRE(pw::equal(generate.values.begin(), generate.values.end(), v.begin(), v.end()));
-
-        WHEN("copy constructor is called")
-        {
-            Vector c(v);
-            THEN("two vectors are same")
-            {
-                REQUIRE(pw::equal(v.begin(), v.end(), c.begin(), c.end()));
-            }
-        }
-    }
-    GIVEN("and add count objects")
-    {
-        size_t const                       count = 5;
-        pw::test::DefaultCopyConstructible copyObject(-1);
-        pw::test::OpCounter                startCount(pw::test::DefaultCopyConstructible::getCounter());
-        Vector                             v(count);
-
-        counter = pw::test::DefaultCopyConstructible::getCounter();
-
-        REQUIRE(v.size() == v.capacity());
-        WHEN("getCounter()")
-        {
-            counter = pw::test::DefaultCopyConstructible::getCounter() - startCount;
-            THEN("count items default constructed")
-            {
-                REQUIRE(count == counter.getDefaultConstructor());
-                REQUIRE(counter.getDefaultConstructor() == counter.constructorCount());
-            }
-        }
-    }
-    counter = pw::test::DefaultCopyConstructible::getCounter() - init;
-    REQUIRE(counter.constructorCount() == counter.destructorCount());
 }
 
-using TestMoveTypeList =
-    std::tuple<pw::vector<pw::test::MoveConstructible>, std::vector<pw::test::MoveConstructible>>;
-TEMPLATE_LIST_TEST_CASE("vector(vector&& other)", "[vector][constructor][moveconstructor]", TestMoveTypeList)
+TEMPLATE_LIST_TEST_CASE("Count constructor", "[phase1][vector][constructor][count]", Phase1TestTypeList)
 {
     using Vector = TestType;
 
-    pw::test::OpCounter const init(pw::test::DefaultCopyConstructible::getCounter());
-    pw::test::OpCounter       counter;
-    GIVEN("A vector with 5 elements")
+    GIVEN("A vector constructed with count only")
     {
-        pw::test::Values<Vector> generate(5);
-        Vector                   v(generate.values);
-
-        WHEN("move constructor is called")
+        WHEN("constructed with size 0")
         {
-            Vector c(v);
-            Vector d(pw::move(v));
-            THEN("two vectors are same")
+            Vector v(0);
+
+            THEN("the vector is empty")
             {
-                REQUIRE(pw::equal(c.begin(), c.end(), d.begin(), d.end()));
-                REQUIRE(c == d);
+                Vector expected{};
+                REQUIRE(v.empty() == true);
+                REQUIRE(v.begin() == v.end());
+                REQUIRE(v == expected);
             }
         }
-        WHEN("save OpCounter before move")
+
+        WHEN("constructed with size 5")
         {
-            Vector c(v);
-            counter = pw::test::DefaultCopyConstructible::getCounter();
-            Vector d(pw::move(v));
-            THEN("move constructor was called")
+            Vector v(5);
+
+            THEN("the vector has 5 default-constructed elements")
             {
+                Vector expected{0, 0, 0, 0, 0};
+                REQUIRE(v.size() == 5);
+                REQUIRE(v.empty() == false);
+                REQUIRE(v.capacity() >= 5);
+                REQUIRE(v == expected);
+            }
+        }
+
+        WHEN("constructed with size 1")
+        {
+            Vector v(1);
+
+            THEN("the vector has 1 default-constructed element")
+            {
+                Vector expected{0};
+                REQUIRE(v.size() == 1);
+                REQUIRE(v.empty() == false);
+                REQUIRE(v.capacity() >= 1);
+                REQUIRE(v == expected);
             }
         }
     }
-    counter = pw::test::DefaultCopyConstructible::getCounter() - init;
-    REQUIRE(counter.constructorCount() == counter.destructorCount());
 }
 
-TEMPLATE_LIST_TEST_CASE("init-list", "[vector][constructor][init-list]", TestTypeList)
+TEMPLATE_LIST_TEST_CASE("Count-value constructor",
+                        "[phase1][vector][constructor][count-value]",
+                        Phase1TestTypeList)
 {
-    using Vector     = TestType;
-    using value_type = typename Vector::value_type;
+    using Vector = TestType;
 
-    pw::test::OpCounter               init(pw::test::DefaultCopyConstructible::getCounter());
-    pw::test::OpCounter               counter;
-    std::initializer_list<value_type> init_list = { 1, 2, 5 };
-
-    counter                                     = pw::test::DefaultCopyConstructible::getCounter() - init;
-    INFO("counter: " << counter);
-    REQUIRE(3 == counter.getOtherConstructor());
-    REQUIRE(3 == counter.constructorCount());
-    GIVEN("A vector from an init list")
+    GIVEN("A vector constructed with count and value")
     {
-        init = pw::test::DefaultCopyConstructible::getCounter();
-        Vector v { init_list };
-        WHEN("nothing is changed")
+        WHEN("constructed with size 0 and value 42")
         {
-            THEN("size() is same")
+            Vector v(static_cast<typename Vector::size_type>(0), 42);
+
+            THEN("the vector is empty")
             {
-                REQUIRE(3 == v.size());
-            }
-            THEN("capacity() is same")
-            {
-                REQUIRE(v.size() == v.capacity());
-            }
-            THEN("constructors called")
-            {
-                counter = pw::test::DefaultCopyConstructible::getCounter() - init;
-                REQUIRE(3 == counter.constructorCount());
-                REQUIRE(3 == counter.getCopyConstructor());
+                Vector expected{};
+                REQUIRE(v.empty() == true);
+                REQUIRE(v.begin() == v.end());
+                REQUIRE(v == expected);
             }
         }
-        WHEN("assign vector")
+
+        WHEN("constructed with size 3 and value 42")
         {
-            Vector v2;
-            v2.push_back(pw::test::DefaultCopyConstructible(1));
-            v2 = v;
-            THEN("They are the same")
+            Vector v(static_cast<typename Vector::size_type>(3), 42);
+
+            THEN("the vector has 3 elements with value 42")
             {
-                REQUIRE(v == v2);
+                Vector expected{42, 42, 42};
+                REQUIRE(v.size() == 3);
+                REQUIRE(v.empty() == false);
+                REQUIRE(v.capacity() >= 3);
+                REQUIRE(v == expected);
             }
         }
-        WHEN("copy construct")
+
+        WHEN("constructed with size 1 and value -7")
         {
-            Vector v2(v);
-            THEN("They are the same")
+            Vector v(static_cast<typename Vector::size_type>(1), -7);
+
+            THEN("the vector has 1 element with value -7")
             {
-                REQUIRE(v == v2);
+                Vector expected{-7};
+                REQUIRE(v.size() == 1);
+                REQUIRE(v.empty() == false);
+                REQUIRE(v.capacity() >= 1);
+                REQUIRE(v == expected);
+            }
+        }
+    }
+}
+
+TEMPLATE_LIST_TEST_CASE("Copy constructor", "[phase1][vector][constructor][copy]", Phase1TestTypeList)
+{
+    using Vector = TestType;
+
+    GIVEN("A source vector")
+    {
+        WHEN("copying an empty vector")
+        {
+            Vector source{};
+            Vector copy(source);
+
+            THEN("the copy is also empty")
+            {
+                Vector expected{};
+                REQUIRE(copy.empty() == true);
+                REQUIRE(copy.begin() == copy.end());
+                REQUIRE(copy == expected);
+                REQUIRE(copy == source);
+            }
+        }
+
+        WHEN("copying a vector with elements")
+        {
+            Vector source{99, 99, 99};
+            Vector copy(source);
+
+            THEN("the copy has the same elements")
+            {
+                Vector expected{99, 99, 99};
+                REQUIRE(copy.size() == source.size());
+                REQUIRE(copy.empty() == source.empty());
+                REQUIRE(copy.capacity() >= source.size());
+                REQUIRE(copy == expected);
+                REQUIRE(copy == source);
+            }
+        }
+
+        WHEN("copying a vector with different values")
+        {
+            Vector source{1, 2, 3, 4};
+            Vector copy(source);
+
+            THEN("the copy has identical content")
+            {
+                Vector expected{1, 2, 3, 4};
+                REQUIRE(copy.size() == 4);
+                REQUIRE(copy == expected);
+                REQUIRE(copy == source);
+            }
+        }
+    }
+}
+
+TEMPLATE_LIST_TEST_CASE("Move constructor", "[phase1][vector][constructor][move]", Phase1TestTypeList)
+{
+    using Vector = TestType;
+
+    GIVEN("A source vector for moving")
+    {
+        WHEN("moving an empty vector")
+        {
+            Vector source{};
+            Vector original_copy(source); // Keep a copy for comparison
+            Vector moved(std::move(source));
+
+            THEN("the moved vector is empty")
+            {
+                Vector expected{};
+                REQUIRE(moved.empty() == true);
+                REQUIRE(moved.begin() == moved.end());
+                REQUIRE(moved == expected);
+                REQUIRE(moved == original_copy);
+            }
+        }
+
+        WHEN("moving a vector with elements")
+        {
+            Vector source{10, 20, 30};
+            Vector original_copy(source); // Keep a copy for comparison
+            Vector moved(std::move(source));
+
+            THEN("the moved vector has the original content")
+            {
+                Vector expected{10, 20, 30};
+                REQUIRE(moved.size() == 3);
+                REQUIRE(moved.empty() == false);
+                REQUIRE(moved == expected);
+                REQUIRE(moved == original_copy);
+            }
+        }
+    }
+}
+
+TEMPLATE_LIST_TEST_CASE("Iterator range constructor",
+                        "[phase1][vector][constructor][range]",
+                        Phase1TestTypeList)
+{
+    using Vector = TestType;
+
+    GIVEN("A source container for iterator range")
+    {
+        WHEN("constructed from empty range")
+        {
+            Vector source{};
+            Vector v(source.begin(), source.end());
+
+            THEN("the vector is empty")
+            {
+                Vector expected{};
+                REQUIRE(v.empty() == true);
+                REQUIRE(v.begin() == v.end());
+                REQUIRE(v == expected);
+                REQUIRE(v == source);
+            }
+        }
+
+        WHEN("constructed from vector range")
+        {
+            Vector source{10, 20, 30, 40};
+            Vector v(source.begin(), source.end());
+
+            THEN("the vector has the same elements")
+            {
+                Vector expected{10, 20, 30, 40};
+                REQUIRE(v.size() == 4);
+                REQUIRE(v.empty() == false);
+                REQUIRE(v == expected);
+                REQUIRE(v == source);
+            }
+        }
+
+        WHEN("constructed from partial range")
+        {
+            Vector source{1, 2, 3, 4, 5};
+            Vector v(source.begin() + 1, source.begin() + 4); // elements 2, 3, 4
+
+            THEN("the vector has the partial range")
+            {
+                Vector expected{2, 3, 4};
+                REQUIRE(v.size() == 3);
+                REQUIRE(v == expected);
+            }
+        }
+
+        WHEN("constructed from array")
+        {
+            int arr[] = {7, 8, 9};
+            Vector v(arr, arr + 3);
+
+            THEN("the vector has the array elements")
+            {
+                Vector expected{7, 8, 9};
+                REQUIRE(v.size() == 3);
+                REQUIRE(v == expected);
             }
         }
     }
