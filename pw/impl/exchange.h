05f18f7239fde04b3b1219f0e9e7ddda2a0391a6
Pete Ware <pware@bloomberg.net>
2025-08-26 20:35:49 -0400

    Adds documentation for `exchange()`

--- pw/impl/exchange.h
+++ pw/impl/exchange.h
@@ -6,12 +6,20 @@
 
 namespace pw {
 
+/**
+ * @brief Replaces the value of obj with new_value and returns the old value of obj.
+ * @tparam Type
+ * @tparam OtherType (defaults to Type) - allows for different but convertible types
+ * @param obj The object whose value is to be replaced.
+ * @param new_value The new value to assign to obj.
+ * @return The old value of obj before the exchange.
+ */
 template<class Type, class OtherType = Type>
 constexpr Type
 exchange(Type& obj, OtherType&& new_value)
 {
     Type old = pw::move(obj);
-    obj      = forward<OtherType>(new_value);
+    obj      = pw::forward<OtherType>(new_value);
     return old;
 }
 
